5f7514628f0092de5f5c33c2b84e023e14debc04
raghu prasad ks <prasadraghuks@gmail.com>
2021-09-03 14:57:13 +0530

    updated

---
+++ day7/3-assignment.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+1. Create a class called Product
+
+having attributes
+
+    code,name,desc,manu,price
+
+having following methods
+
+    addProduct(self,code,name,desc,manu,price)
+    
+    displayProduct(self)
+    return product attributes
+    
+2. show the product having maximum price
+3. show all products of a specific manufacturer
+4. search for a specific product
+
+"""
+
+
